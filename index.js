608257fce77517845b19ebe42ad1b320b45a61f7
Tinvuong <148146620+Tinvuong@users.noreply.github.com>
2023-10-25 00:21:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function onClick(){
     let a = parseInt(document.getElementById("aVar").value);
     let b = parseInt(document.getElementById("bVar").value);
     let c = a+b;
+    if(isNaN(c)){
+        c = "Please enter number!"
+    }
     document.getElementById("result").innerHTML = "Result: "+ c;
 }
